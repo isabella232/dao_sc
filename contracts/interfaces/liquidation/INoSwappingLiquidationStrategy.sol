b672d4f1dccce5fd1ee4936a6e5a976ccbd84a63
Mike <manhlx.300695@gmail.com>
2021-04-13 17:49:50 +0700

    Rebase from katana

--- contracts/interfaces/liquidation/INoSwappingLiquidationStrategy.sol
+++ contracts/interfaces/liquidation/INoSwappingLiquidationStrategy.sol
@@ -5,13 +5,13 @@ import {IERC20Ext} from '@kyber.network/utils-sc/contracts/IERC20Ext.sol';
 
 
 interface INoSwappingLiquidationStrategy {
-  event FeePoolSet(address indexed feePool);
   event TreasuryPoolSet(address indexed treasuryPool);
+  event RewardPoolSet(address indexed rewardPool);
   event Liquidated(address sender, IERC20Ext[] sources, uint256[] amounts);
 
-  function updateFeePool(address pool) external;
-  function updateTreasuryPool(address payable pool) external;
+  function updateTreasuryPool(address pool) external;
+  function updateRewardPool(address payable pool) external;
   function liquidate(IERC20Ext[] calldata sources, uint256[] calldata amounts) external;
-  function feePool() external view returns (address);
   function treasuryPool() external view returns (address);
+  function rewardPool() external view returns (address);
 }
--- test/noSwappingLiquidationStrategy.js
+++ test/noSwappingLiquidationStrategy.js
@@ -10,11 +10,11 @@ const {zeroAddress, ethAddress} = require('./helper.js');
 
 let admin;
 let operator;
-let feePool;
 let treasuryPool;
+let rewardPool;
 let strategy;
 
-contract('KyberGovernance', function (accounts) {
+contract('NoSwappingLiquiditionStrategy', function (accounts) {
   before('Global setup', async () => {
     admin = accounts[1];
     operator = accounts[4];
@@ -23,15 +23,15 @@ contract('KyberGovernance', function (accounts) {
   describe('#constructor', async () => {
     it('invalid params', async () => {
       await expectRevert(NoSwappingLiquidationStrategy.new(zeroAddress, accounts[0], accounts[0]), 'admin 0');
-      await expectRevert(NoSwappingLiquidationStrategy.new(admin, zeroAddress, accounts[0]), 'invalid fee pool');
-      await expectRevert(NoSwappingLiquidationStrategy.new(admin, accounts[0], zeroAddress), 'invalid treasury pool');
+      await expectRevert(NoSwappingLiquidationStrategy.new(admin, zeroAddress, accounts[0]), 'invalid treasury pool');
+      await expectRevert(NoSwappingLiquidationStrategy.new(admin, accounts[0], zeroAddress), 'invalid reward pool');
     });
 
     it('correct data inited', async () => {
       strategy = await NoSwappingLiquidationStrategy.new(admin, accounts[0], accounts[2]);
       Helper.assertEqual(admin, await strategy.admin());
-      Helper.assertEqual(accounts[0], await strategy.feePool());
-      Helper.assertEqual(accounts[2], await strategy.treasuryPool());
+      Helper.assertEqual(accounts[0], await strategy.treasuryPool());
+      Helper.assertEqual(accounts[2], await strategy.rewardPool());
     });
   });
 
@@ -40,27 +40,11 @@ contract('KyberGovernance', function (accounts) {
       strategy = await NoSwappingLiquidationStrategy.new(admin, accounts[0], accounts[2]);
     });
 
-    it('fee pool - reverts not admin', async () => {
-      await expectRevert(strategy.updateFeePool(accounts[2], {from: operator}), 'only admin');
-    });
-
-    it('fee pool - reverts invalid fee pool', async () => {
-      await expectRevert(strategy.updateFeePool(zeroAddress, {from: admin}), 'invalid fee pool');
-    });
-
-    it('fee pool - data updates and events', async () => {
-      let tx = await strategy.updateFeePool(accounts[5], {from: admin});
-      Helper.assertEqual(accounts[5], await strategy.feePool());
-      expectEvent(tx, 'FeePoolSet', {
-        feePool: accounts[5],
-      });
-    });
-
     it('treasury pool - reverts not admin', async () => {
       await expectRevert(strategy.updateTreasuryPool(accounts[2], {from: operator}), 'only admin');
     });
 
-    it('treasury pool - reverts invalid fee pool', async () => {
+    it('treasury pool - reverts invalid reward pool', async () => {
       await expectRevert(strategy.updateTreasuryPool(zeroAddress, {from: admin}), 'invalid treasury pool');
     });
 
@@ -71,42 +55,58 @@ contract('KyberGovernance', function (accounts) {
         treasuryPool: accounts[5],
       });
     });
+
+    it('reward pool - reverts not admin', async () => {
+      await expectRevert(strategy.updateRewardPool(accounts[2], {from: operator}), 'only admin');
+    });
+
+    it('reward pool - reverts invalid reward pool', async () => {
+      await expectRevert(strategy.updateRewardPool(zeroAddress, {from: admin}), 'invalid reward pool');
+    });
+
+    it('reward pool - data updates and events', async () => {
+      let tx = await strategy.updateRewardPool(accounts[5], {from: admin});
+      Helper.assertEqual(accounts[5], await strategy.rewardPool());
+      expectEvent(tx, 'RewardPoolSet', {
+        rewardPool: accounts[5],
+      });
+    });
   });
 
   describe('#liquidate', async () => {
     let tokens = [];
     before('deploy tokens', async () => {
-      feePool = await Pool.new(admin, []);
       treasuryPool = await Pool.new(admin, []);
+      rewardPool = await Pool.new(admin, []);
       for (let i = 0; i < 4; i++) {
         let token = await Token.new();
         tokens.push(token);
       }
     });
 
-    it('revert withdraw funds from fee pool reverts', async () => {
+    it('revert withdraw funds from treasury pool reverts', async () => {
       strategy = await NoSwappingLiquidationStrategy.new(admin, accounts[2], accounts[3]);
       await expectRevert.unspecified(strategy.liquidate([tokens[0].address], [10]));
     });
 
     it('correct event', async () => {
-      strategy = await NoSwappingLiquidationStrategy.new(admin, feePool.address, treasuryPool.address);
+      strategy = await NoSwappingLiquidationStrategy.new(admin, treasuryPool.address, rewardPool.address);
       let ethAmount = new BN(10000);
       let tokenAmount = new BN(2000);
-      await Helper.sendEtherWithPromise(accounts[0], feePool.address, ethAmount);
-      await tokens[0].transfer(feePool.address, tokenAmount);
-      let feeEthBal = await Helper.getBalancePromise(feePool.address);
+      await Helper.sendEtherWithPromise(accounts[0], treasuryPool.address, ethAmount);
+      await tokens[0].transfer(treasuryPool.address, tokenAmount);
       let treasuryEthBal = await Helper.getBalancePromise(treasuryPool.address);
-      let feeTokenBal = await tokens[0].balanceOf(feePool.address);
+      let rewardEthBal = await Helper.getBalancePromise(rewardPool.address);
       let treasuryTokenBal = await tokens[0].balanceOf(treasuryPool.address);
-      await feePool.authorizeStrategies([strategy.address], {from: admin});
+      let rewardTokenBal = await tokens[0].balanceOf(rewardPool.address);
+      await treasuryPool.authorizeStrategies([strategy.address], {from: admin});
       let tx = await strategy.liquidate([ethAddress, tokens[0].address], [ethAmount, tokenAmount], {
         from: accounts[4],
       });
-      Helper.assertEqual(feeEthBal.sub(ethAmount), await Helper.getBalancePromise(feePool.address));
-      Helper.assertEqual(treasuryEthBal.add(ethAmount), await Helper.getBalancePromise(treasuryPool.address));
-      Helper.assertEqual(feeTokenBal.sub(tokenAmount), await tokens[0].balanceOf(feePool.address));
-      Helper.assertEqual(treasuryTokenBal.add(tokenAmount), await tokens[0].balanceOf(treasuryPool.address));
+      Helper.assertEqual(treasuryEthBal.sub(ethAmount), await Helper.getBalancePromise(treasuryPool.address));
+      Helper.assertEqual(rewardEthBal.add(ethAmount), await Helper.getBalancePromise(rewardPool.address));
+      Helper.assertEqual(treasuryTokenBal.sub(tokenAmount), await tokens[0].balanceOf(treasuryPool.address));
+      Helper.assertEqual(rewardTokenBal.add(tokenAmount), await tokens[0].balanceOf(rewardPool.address));
       let hasEvent = false;
       for (let i = 0; i < tx.logs.length; i++) {
         if (tx.logs[i].event == 'Liquidated') {
