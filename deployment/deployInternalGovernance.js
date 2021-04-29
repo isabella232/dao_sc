cb1abdaacc897c72f24c855ad703b40da6cd0bb9
Mike <manhlx.300695@gmail.com>
2021-04-29 11:16:50 +0700

    Update deploy script

--- deployment/deployInternalGovernance.js
+++ deployment/deployInternalGovernance.js
@@ -10,12 +10,7 @@ async function verifyContract(hre, contractAddress, ctorArgs) {
 }
 
 const operators = [
-  "0xbe2f0354d970265bfc36d383af77f72736b81b54", // Mike
-  "0xf214dDE57f32F3F34492Ba3148641693058D4A9e", // Victor
-  "0xdE6BBD964b9D0148d46FE6e2E9Cf72B020ADc519", // Sunny
-  "0xf3d872b9e8d314820dc8e99dafbe1a3feedc27d5", // Spyros
-  "0x417446168952735b8f51dF840a1838AE78104558", // Shane
-  "0x5565d64f29Ea17355106DF3bA5903Eb793B3e139", // Loi
+
 ]
 
 const admin = "0x3eb01b3391ea15ce752d01cf3d3f09dec596f650";
@@ -32,10 +27,10 @@ task('deployInternalGov', 'deploy internal governance for Kyber').setAction(
     console.log(`Deployer address: ${deployerAddress}`)
 
     // contract deployment
-     gasPrice = new BN.from(32).mul(new BN.from(10).pow(new BN.from(18)));
+     gasPrice = new BN.from(32).mul(new BN.from(10).pow(new BN.from(9)));
     const KyberInternalGovernance = await ethers.getContractFactory('KyberInternalGovernance');
+    let addresses = [];
     for(let i = 0; i < operators.length; i++) {
-      console.log(`Deploying for operator: ${operators[i]}`)
       let contract = await KyberInternalGovernance.deploy(
         admin, rewardRecipient, governance, rewardDistributor, operators[i],
       {
@@ -43,9 +38,13 @@ task('deployInternalGov', 'deploy internal governance for Kyber').setAction(
       });
       await contract.deployed();
       console.log(`Internal governance for ${operators[i]} address: ${contract.address}`);
-      console.log('Verifying contracts...');
+      addresses.push(contract.address);
+    }
+
+    for(let i = 0; i < addresses.length; i++) {
+      console.log(`Verifying contracts ${addresses[i]}`);
       // verify addresses
-      await verifyContract(hre, contract.address, [admin, rewardRecipient, governance, rewardDistributor, operators[i]]);
+      await verifyContract(hre, addresses[i], [admin, rewardRecipient, governance, rewardDistributor, operators[i]]);
     }
     console.log('setup completed');
     process.exit(0);
